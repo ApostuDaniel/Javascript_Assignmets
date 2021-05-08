f2fc75815262aee7a97bcca9f9062b5413ba4ecf
ApostuDaniel <79244856+ApostuDaniel@users.noreply.github.com>
2021-05-08 21:34:13 +0300

    Now uses mangoDB with mangoose

--- Assingnment5/webapi/web.js
+++ Assingnment5/webapi/web.js
@@ -3,39 +3,48 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-fs.closeSync(fs.openSync('data.json', 'a'))
-const unformated = fs.readFileSync('data.json');
-let myList = [];
-if(unformated.length !== 0){
-    let formated = JSON.parse(unformated);
-    myList = [...formated];
-}
+const mongoose = require("mongoose")
+const DBurl = "mongodb+srv://<username>:<password>@cluster0.6dca8.mongodb.net/<yourDatabase>?retryWrites=true&w=majority"
+mongoose.connect(DBurl, {useNewUrlParser : true, useUnifiedTopology : true});
+const db = mongoose.connection;
+db.on('error', () => {console.log("Could not connect")});
 
-app.get('/products', (req, res) =>{
+const productModel = new mongoose.model('products', new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    inStock: Boolean
+}));
+
+app.get('/products', async (req, res) =>{
+    const myList = await productModel.find({});
     res.send(myList);
 });
 
-app.get('/products/:productId', (req, res)=>{
+app.get('/products/:productId', async (req, res)=>{
     const id = req.params.productId;
+    const myList = await productModel.find({});
     res.send(myList[id]);
 });
 
-app.post('/products', (req, res) =>{
+app.post('/products', async (req, res) =>{
     const parameters = req.query;
-    if (!parameters.name || myList.some(arrObj => arrObj.name === parameters.name))
+    const sameName = await productModel.findOne({name: `${parameters.name}`});
+    if (!parameters.name || sameName)
     {
         res.send(false);
     }
     else{
-        myList.push({name: parameters.name, description: parameters.description, price: parameters.price, inStock: parameters.inStock});
-        fs.writeFileSync('data.json', JSON.stringify(myList));
+        const newProd = new productModel({name: parameters.name, description: parameters.description, price: parameters.price, inStock: parameters.inStock});
+        await newProd.save();
         res.send(true);
     }
 });
 
-app.patch('/products/:productId', (req, res)=>{
+app.patch('/products/:productId', async(req, res)=>{
     const id = req.params.productId;
     const parameters = req.query;
+    const myList = await productModel.find({});
     let validName = true;
     if(parameters.name){
         for(let i = 0; i < myList.length; ++i)
@@ -50,11 +59,14 @@ app.patch('/products/:productId', (req, res)=>{
     else validName = false;
     if(validName)
     {
-        myList[id].name = parameters.name;
-        myList[id].description = parameters.description;
-        myList[id].price = parameters.price;
-        myList[id].inStock = parameters.inStock;
-        fs.writeFileSync('data.json', JSON.stringify(myList));
+        const query = {name: `${myList[id].name}`};
+        await productModel.findOneAndUpdate(query, 
+            {
+                name: `${parameters.name}`,
+                description: `${parameters.description}`,
+                price: parameters.price,
+                inStock: parameters.inStock
+            });
         res.send(true);
     }
     else
@@ -63,14 +75,15 @@ app.patch('/products/:productId', (req, res)=>{
     }
 });
 
-app.delete('/products/:productId', (req, res) =>{
+app.delete('/products/:productId', async(req, res) =>{
     const id = req.params.productId;
-    myList.splice(id, 1);
-    fs.writeFileSync('data.json', JSON.stringify(myList));
+    const myList = await productModel.find({});
+    await productModel.findOneAndDelete({ name: `${myList[id].name}`});
     res.send(true); 
 });
 
-app.listen(3000);
+db.once('open', async () =>{app.listen(3000)});
+
 
 
 
